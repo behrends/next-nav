666aad2dd651d767aec0fe52c0b26c1432e5f4ae
Erik Behrends <behrends@users.noreply.github.com>
2023-05-09 13:43:58 +0200

    Change heading in course page

--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -3,7 +3,7 @@ import Courses from '@/components/Courses';
 export default function CourseScreen() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl">Kurse</h1>
+      <h1 className="text-4xl">Kurse im SZI</h1>
       <Courses />
     </main>
   );
