5dc08a8124859ca5854687ce2d106653d45364ce
Erik Behrends <behrends@users.noreply.github.com>
2024-05-07 19:15:28 +0200

    Configure routes as object and map to Links

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,39 +3,39 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 
+const appRoutes = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/playground',
+    label: 'Playground',
+  },
+  {
+    href: '/settings',
+    label: 'Einstellungen',
+  },
+];
+
 export default function Nav() {
   // determine the current page with hook
   const pathname = usePathname();
   return (
     <header className="sticky top-0 flex items-center h-16 border-b bg-background px-6">
       <nav className="flex items-center gap-6 text-sm font-medium">
-        <Link
-          href="/"
-          className={cn(
-            'text-muted-foreground transition-colors hover:text-foreground',
-            { 'text-foreground': pathname === '/' }
-          )}
-        >
-          Home
-        </Link>
-        <Link
-          href="playground"
-          className={cn(
-            'text-muted-foreground transition-colors hover:text-foreground',
-            { 'text-foreground': pathname === '/playground' }
-          )}
-        >
-          Playground
-        </Link>
-        <Link
-          href="settings"
-          className={cn(
-            'text-muted-foreground transition-colors hover:text-foreground',
-            { 'text-foreground': pathname === '/settings' }
-          )}
-        >
-          Einstellungen
-        </Link>
+        {appRoutes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              'text-muted-foreground transition-colors hover:text-foreground',
+              { 'text-foreground': route.href === pathname }
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
       </nav>
     </header>
   );
