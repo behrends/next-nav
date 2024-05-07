f95e51b017e5667810bdffd86186641582fa779b
Erik Behrends <behrends@users.noreply.github.com>
2024-05-07 07:58:41 +0200

    Install and use Card component from shadcn

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Next.js App</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Next.js App</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Dies ist eine Beispiel-App mit Next.js, Tailwind und
+            shadcn/ui
+          </p>
+        </CardContent>
+      </Card>
     </main>
   );
 }
