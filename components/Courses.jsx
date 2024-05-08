07f5e350c30038c99c3b1c73dd016c1924cbd7de
Erik Behrends <behrends@users.noreply.github.com>
2024-05-08 12:31:55 +0200

    Format courses with cards

--- components/Courses.jsx
+++ components/Courses.jsx
@@ -1,3 +1,5 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+
 export default async function Courses() {
   // this fetch will run server side (as this is a server component)!
   const data = await fetch(
@@ -6,12 +8,37 @@ export default async function Courses() {
 
   const courses = await data.json();
   return (
-    <>
-      {courses.map((course) => (
-        <div key={course.id}>
-          <h2>{course.name}</h2>
-        </div>
-      ))}
-    </>
+    <div className="py-2">
+      <h2 className="text-xl">Liste von Beispiel-Kursen</h2>
+      <p className="mb-4">
+        Die folgenden Kurse werden aus der Datei <code>db.json</code>{' '}
+        (
+        <a
+          href="https://github.com/behrends/next-nav"
+          target="_blank"
+          className="text-blue-500"
+        >
+          &rarr; GitHub-Repository
+        </a>
+        ) via{' '}
+        <a
+          href="https://my-json-server.typicode.com"
+          target="_blank"
+          className="text-blue-500"
+        >
+          my-json-server
+        </a>{' '}
+        geladen.
+      </p>
+      <div className="grid grid-cols-3 gap-3">
+        {courses.map((course) => (
+          <Card key={course.id}>
+            <CardHeader>
+              <CardTitle>{course.name}</CardTitle>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 }
